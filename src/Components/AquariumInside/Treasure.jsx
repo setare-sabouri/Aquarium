666750e893cc92a191cf2788d4315a1d37e80b6d
setareh <setarehsaboory@gmail.com>
2025-08-28 04:45:04 +0200

    starfish

--- src/Components/AquariumInside/Treasure.jsx
+++ src/Components/AquariumInside/Treasure.jsx
@@ -1,6 +1,7 @@
-import { Sphere } from '@react-three/drei';
+import { Sphere, useGLTF } from '@react-three/drei';
 
 const Treasure = () => {
+  const star =useGLTF('./models/starfish.glb')
   return (
     <Sphere args={[0.2, 16, 16]} position={[0, 0.3, 0]}>
       <meshStandardMaterial color="gold" />
